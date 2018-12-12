72b7607f445379e0936c93f8262a4f90319c9ecc
Franco Geller <franco.geller@gmail.com>
2018-12-12 11:59:15 -0300

    todo 2

--- src/test/java/ar/edu/iua/ingweb3proyecto/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.iua.ingweb3proyecto;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = ("ar.edu.iua.ingweb3final"))
-public class AppConfig {
-}
